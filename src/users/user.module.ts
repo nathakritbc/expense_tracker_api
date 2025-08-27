0da102769e71073c94434d0a572337100a3f5e3f
Nathakrit Tato🐷 <nathakritbc602@gmail.com>
2025-08-27 23:10:46 +0700

    refactor:💡 simplify user module by removing unused use cases and tests, update example email in auth requests

--- src/users/user.module.ts
+++ src/users/user.module.ts
@@ -1,17 +1,3 @@
 import { Module } from '@nestjs/common';
-import { UserTypeOrmRepository } from './adapters/outbounds/user.typeorm.repository';
-import { userRepositoryToken } from './applications/ports/user.repository';
-import { CreateUserUseCase } from './applications/usecases/createUser.usecase';
-import { GetUserByEmailUseCase } from './applications/usecases/getUserByEmail.usecase';
-
-@Module({
-  providers: [
-    {
-      provide: userRepositoryToken,
-      useClass: UserTypeOrmRepository,
-    },
-    CreateUserUseCase,
-    GetUserByEmailUseCase,
-  ],
-})
+@Module({})
 export class UserModule {}
